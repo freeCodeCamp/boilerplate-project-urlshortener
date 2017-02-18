13dd4b63761292c69d750d88e0ddaf529aba2273
System <none>
2017-02-18 18:57:45 +0000

    Initial commit from Gomix.

---
+++ server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+var cors = require('cors');
+
+var app = express();
+
+// Basic Configuration 
+var port = process.env.PORT || 3000;
+
+/** this project needs a db !! **/ 
+// mongoose.connect(process.env.MONGOLAB_URI);
+
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+  
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+app.listen(port, function () {
+  console.log('Node.js listening ...');
+});
