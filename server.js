1a2f384ffe2d35d52b42d78a2c2ff4613ee0b5fd
Brandon <brandon.g.elzy@gmail.com>
2019-02-03 14:56:22 -0600

    Brandon G. Elzy - Initial Commit

--- server.js
+++ server.js
@@ -1,37 +1,35 @@
 'use strict';
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const express = require('express');
+const server = express();
+const PORT = process.env.PORT || 3000;
 
-var cors = require('cors');
+const cors = require('cors');
 
-var app = express();
 
-// Basic Configuration 
-var port = process.env.PORT || 3000;
-
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
 // mongoose.connect(process.env.MONGOLAB_URI);
 
-app.use(cors());
+server.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
-app.use('/public', express.static(process.cwd() + '/public'));
+server.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+server.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+
+// your first API endpoint...
+server.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+server.listen(PORT, function () {
+  console.log(`Server listening on port ${PORT}...`);
 });
