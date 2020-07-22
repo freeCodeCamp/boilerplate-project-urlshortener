a0556d30ee1d37d816889fff8ac99f021330b41c
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2020-07-22 14:36:06 +0900

    feat: cleanup glitch assets and files and general prettifying

--- server.js
+++ server.js
@@ -1,37 +1,31 @@
-'use strict';
+const express = require('express');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+// Basic Configuration
+const port = process.env.PORT || 3000;
 
-var cors = require('cors');
-
-var app = express();
-
-// Basic Configuration 
-var port = process.env.PORT || 3000;
-
-/** this project needs a db !! **/ 
+/* This project needs a database */
 // mongoose.connect(process.env.DB_URI);
 
 app.use(cors());
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+/* This project needs to parse POST bodies */
+// You should mount body-parser here
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
-
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
 });
