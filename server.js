49cec8805b0b07ee3f72182ab820e03cd5cea1e8
zeezatcoder <92806158+zeezatcoder@users.noreply.github.com>
2022-01-23 23:25:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const bodyParser = require('body-parser');
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,10 +17,47 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const links = [];
+let id = 0;
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/shorturl/new', function(req, res) {
+  const { url } = req.body;
+  const noHTTPSurl = url.replace(/^https?:\/\//, '');
+
+
+  dns.lookup(noHTTPSurl, (err) => {
+    if(err) {
+      return res,json({
+        error: "invalid URL"
+      });
+    } else {
+      id++;
+      const link = {
+        original_url: url,
+        short_url: `${id}`
+      };
+
+      links.push(link);
+      console.log(link);
+      return res.json(link);
+  }
+  });
 });
+
+app.get('/api/shorturl/:id', (req, res) => {
+  const { id } = req.params;
+
+   const link = links.find(n => n.short_url === id);
+
+   if(link) {
+   return res.redirect(link.original_url);
+   } else {
+     return res.json({
+       error: 'No short url'
+     });
+   }
+   })
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
