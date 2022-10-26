65c0be0e1f20636669fc582c24819f1349a91a08
Juliano Alfredo <juliano.alfredo86@gmail.com>
2022-10-26 16:43:49 -0300

    finish

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const shortid = require('shortid')
+const dotenv = require('dotenv').config()
+var app = express()
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -14,10 +21,46 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.post('/api/shorturl/new', async function(req, res){
+  const {url} = req.body
+  const uid = shortid.generate()
+  console.log(validURL.isUri(url));
+  if (validURL.isWebUri(url) === undefined) {
+    res.json({
+      error: 'invalid url',
+    });
+  } else{
+    console.log(url)
+    client.connect( async err =>{
+       if (err) throw err
+       await client.db('url-db').collection('url-config').insertOne({
+         "url": `${url}`,
+         "short_id": `${uid}`,
+       })
+       res.send({
+         "url": `${url}`,
+         "short_id": `${uid}`,
+       })
+      })
+  }
+   
+})
+app.get("/api/shorturl/:shorturl", async function(req, res){
+  client.connect(async err =>{
+      if(err) throw err
+      await client.db("url-db").collection("url-config").find({}).toArray(async function(err, result){
+          if(err) throw err;
+          result.forEach((obj) =>{
+              if(obj.short_id == req.params.shorturl){
+                  res.redirect(obj.url)
+              } else{
+                  console.log("PASSED")
+              }
+          })
+      })
+  })
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
