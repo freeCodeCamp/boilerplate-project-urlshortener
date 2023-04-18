f991e75c8f6ebfc2ebd9e9a5328a19029147aacc
Ali Ebrahimi <aliebrahimi2150@gmail.com>
2023-04-18 23:42:02 +0330

    feat: add post url endpoint
    feat: add global validation error handler

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const bodyparser = require('body-parser');
+
+const Url = require('./models/url');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI).then(()=>{
-  console.log('db connected!')
-})
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log('db connected!');
+});
+
+app.use(bodyparser.urlencoded({ extended: false }));
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.post('/api/shorturl', async (req, res, next) => {
+  try {
+    const inputUrl = req.body.url;
+    const url = await new Url({ url: inputUrl }).saveUrl();
+    res.json({
+      original_url: url.url,
+      short_url: url.short_url,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err.name === 'ValidationError') {
+    return res.json({
+      // getting first error message
+      error: err.errors[Object.keys(err.errors)[0]].message,
+    });
+  }
+  next(err);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
--- models/url.js
+++ models/url.js
@@ -1,35 +1,69 @@
 const mongoose = require('mongoose');
-const dns = require('node:dns')
+const dns = require('node:dns');
 
-const urlSchema = new mongoose.Schema({
-  url : {
-    type: String,
-    required: true,
-    validate : {
-      validator : urlValidator,
-      message : "invalid url"
+const urlSchema = new mongoose.Schema(
+  {
+    url: {
+      type: String,
+      required: true,
+      validate: {
+        validator: urlValidator,
+        message: 'invalid url',
+      },
+      unique: true,
     },
-    unique: true
+    short_url: {
+      type: Number,
+    },
   },
-  short_url : {
-    type : Number
+  {
+    methods: {
+      saveUrl,
+    },
   }
-});
+);
 
-function urlValidator(v){
-  const urlPattern = /^(https?:\/\/)(.*)$/ 
-  return new Promise((resolve,reject)=>{
-    if(!v.match(urlPattern)){
+function urlValidator(v) {
+  const urlPattern = /^(https?:\/\/)(.*)$/;
+  return new Promise((resolve, reject) => {
+    if (!v.match(urlPattern)) {
       resolve(false);
     }
-    dns.lookup(v.match(urlPattern)[2],(err)=>{
-      if(err || !v.match()){
+    dns.lookup(v.match(urlPattern)[2], (err) => {
+      if (err || !v.match()) {
         resolve(false);
       }
       resolve(true);
-    })
-  })
+    });
+  });
 }
 
+async function saveUrl() {
+  const Url = this.constructor;
+  const urlObj = await Url.findOne({ url: this.url });
+  if (urlObj) {
+    return urlObj;
+  }
+  return this.save();
+}
+
+urlSchema.statics.findLatestShortUrl = async function () {
+  const latestUrl = await this.findOne(
+    {},
+    { short_url: 1 },
+    {
+      sort: {
+        short_url: -1,
+      },
+    }
+  );
+  return latestUrl ? latestUrl['short_url'] : 0;
+};
+
+urlSchema.pre('save', async function (next) {
+  const Url = this.constructor;
+  this.short_url = (await Url.findLatestShortUrl()) + 1;
+  next();
+});
 
 module.exports = mongoose.model('Url', urlSchema);
