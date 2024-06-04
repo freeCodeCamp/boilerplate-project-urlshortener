8f45e5b40461eda08b0d0805fc0a3d5b8a3e64b8
Developer Siddarth <siddarthabcs@gmail.com>
2024-06-04 18:56:37 +0000

    done

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
+const dns = require('dns');
+const { error } = require('console');
+const urlModule = require('url');
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+let urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: Number
+});
+
+let urls = mongoose.model('urls', urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,8 +33,34 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  console.log(typeof(req.body.url))
+  let url = req.body.url;
+  dns.lookup( urlModule.parse(url).hostname , async( err , address ) => {
+    if(!address) {
+      res.json({ error: 'invalid url' });
+    } else {
+      const urlCount = await urls.countDocuments({})
+      const urlDoc = {
+        original_url: url,
+        short_url: urlCount
+      }
+
+      const result = await urls.create(urlDoc)
+      console.log(result);
+      res.json({ original_url: url, short_url: urlCount })
+    }
+  });
+});
+
+app.get('/api/shorturl/:short_url', async (req,res)=>{
+  const shorturl = req.params.short_url;
+  const urlDoc = await urls.findOne({short_url: +shorturl})
+  if(urlDoc){
+    res.redirect(urlDoc.original_url)
+  }else{
+    res.json({Error: 'No URL found for the given short URL'})
+  }
 });
 
 app.listen(port, function() {
