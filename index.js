674755fc7762f411f17826e6550a2c0f545433cb
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2024-09-03 15:03:26 +0530

    fix public assets folder and index.html file not working in vercel

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use('/public', express.static(__dirname + "/public"));
 
 app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile( __dirname + '/views/index.html');
 });
 
 // Your first API endpoint
